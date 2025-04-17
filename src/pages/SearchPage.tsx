d260e0bcb13e7e37e0c0463e8bd0cecee009bebf
WhirlwindWar <gaglianopa@gmail.com>
2025-04-16 23:50:19 -0400

    start search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import CircularProgress from '@mui/material/CircularProgress';
+import '../style/pages/SearchPage.scss';
 
 const SearchPage: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [inputValue, setInputValue] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const onTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+        setLoading(true);
+    }
+
     return (
-        <div>
-            <h2>Search</h2>
-            <p>coming soon</p>
-        </div>
+        <>
+            <div className="input-wrapper">
+                <SearchIcon className="input-icon"/>
+                <input
+                    ref={inputRef}
+                    className="input"
+                    placeholder="Search for Recipes"
+                    value={inputValue}
+                    onChange={onTextChange}
+                />
+                {loading && <CircularProgress className="loading-indicator" size={24} thickness={5}/>}
+            </div>
+        </>
     );
 };
 
