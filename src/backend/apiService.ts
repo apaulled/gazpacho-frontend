9e0c1105b22733e97c24209756389f14ac44b2ab
WhirlwindWar <gaglianopa@gmail.com>
2025-04-18 02:31:48 -0400

    implement profile page

--- src/backend/apiService.ts
+++ src/backend/apiService.ts
@@ -10,6 +10,12 @@ export interface RecipeResponse {
     image: string;
 }
 
+export interface UserResponse {
+    id: number;
+    email: string;
+    savedRecipeIds: number[];
+}
+
 export const fetchRecipe = async (recipeId: string): Promise<RecipeResponse> => {
     try {
         const response: AxiosResponse<RecipeResponse> = await api.get(`/v1/recipes/${recipeId}`);
@@ -27,3 +33,12 @@ export const searchRecipes = async (query: string): Promise<RecipeResponse[]> =>
         throw new Error(`Error searching recipes: ${error}`);
     }
 };
+
+export const getUser = async (): Promise<UserResponse> => {
+    try {
+        const response: AxiosResponse<UserResponse> = await api.get(`/v1/users`);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching user data: ${error}`);
+    }
+}
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import {RecipeResponse, UserResponse} from "../backend/apiService";
+import RecipeCard from "../components/RecipeCard";
+import profileImage from "../resources/gazpacho_profile.png";
+import '../style/pages/ProfilePage.scss';
+
+const dummyUser: UserResponse = {
+    id: 1,
+    email: "fake@gmail.com",
+    savedRecipeIds: [1, 2, 3],
+}
+
+const dummyRecipes: RecipeResponse[] = [
+    {
+        id: 1,
+        name: 'Spaghetti Bolognese',
+        steps: [],
+        ingredients: [],
+        allergens: ['Gluten'],
+        image: 'https://img.taste.com.au/5qlr1PkR/taste/2016/11/spaghetti-bolognese-106560-1.jpeg',
+    },
+    {
+        id: 2,
+        name: 'Chicken Salad',
+        steps: [],
+        ingredients: [],
+        allergens: ['Nuts'],
+        image: 'https://kalejunkie.com/wp-content/uploads/2023/04/ChickenSalad_Shot4_121.jpg',
+    },
+    {
+        id: 3,
+        name: 'Vegetable Stir Fry',
+        steps: [],
+        ingredients: [],
+        allergens: [],
+        image: 'https://kristineskitchenblog.com/wp-content/uploads/2024/01/vegetable-stir-fry-22-3.jpg',
+    },
+]
 
 const ProfilePage: React.FC = () => {
     return (
-        <div>
-            <h2>Profile</h2>
-            <p>coming soon</p>
+        <div className="profile-container">
+            <div className="profile-header">
+                <img src={profileImage} alt="Profile Placeholder" className="profile-image"/>
+                <div className="profile-info">
+                    <h1 className="fancy-text">{dummyUser.email}</h1>
+                    <h2 className="fancy-text">Saved Recipes: {dummyRecipes.length}</h2>
+                </div>
+            </div>
+            <div className="saved-recipes">
+                {dummyRecipes.map(recipe => (
+                    <RecipeCard
+                        key={recipe.id}
+                        id={recipe.id}
+                        name={recipe.name}
+                        allergens={recipe.allergens}
+                        image={recipe.image}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
